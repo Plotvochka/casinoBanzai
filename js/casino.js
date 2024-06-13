64b1a951bd9fda9eba82b96edba62a7510134950
Plotvochka <106927858+Plotvochka@users.noreply.github.com>
2024-06-13 14:52:46 +0300

    done

--- js/casino.js
+++ js/casino.js
@@ -1,5 +1,46 @@
 "use strict";
 
+const scrollUp = document.querySelector(".scroll-up");
+
+scrollUp.addEventListener("click", scrollTop);
+window.addEventListener("scroll", trackScroll);
+
+function trackScroll() {
+  const offset = window.pageYOffset;
+  const coords = document.documentElement.clientHeight;
+  if (offset > coords) {
+    scrollUp.classList.add("active-up");
+  } else {
+    scrollUp.classList.remove("active-up");
+  }
+}
+
+function scrollTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -100000000000);
+  }
+}
+
+const menu = document.querySelector(".burger-wrap");
+const backDrop = document.querySelector(".backdrop");
+const closeBtn = document.querySelector(".close-btn");
+
+menu.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+
+function openModal() {
+  if (!backDrop.classList.contains("is-open")) {
+    backDrop.classList.add("is-open");
+    document.body.style.overflow = "hidden";
+  }
+}
+function closeModal() {
+  if (backDrop.classList.contains("is-open")) {
+    backDrop.classList.remove("is-open");
+    document.body.style.overflow = "visible";
+  }
+}
+
 const nav = document.querySelectorAll(".nav-title");
 nav.forEach((el) => {
   el.addEventListener("click", navOpen);
